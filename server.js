d2fa57010cde83d1c767acabbf620986f72de4c7
karosi <adkayor21@gmail.com>
2016-11-07 16:51:22 +0100

    Making little change

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var path = require('path')
 var moment = require('moment')
 var app = express();
 
-app.get("/",function(req,res) {
-	res.send("Hello World!");
-}).listen(3000,function(){
-	console.log("Server running on port: "+3000);
+app.set('port',process.env.PORT || 3000)
+app.use(express.static(path.resolve(__dirname,'public')));
+app.get("/", (req,res) => {
+	res.end();
+}).listen(app.get('port'), () => {
+	console.log(`Server running on port ${app.get('port')}`);
 });
