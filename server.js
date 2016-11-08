2cd1b389770896ac3a77025ca92ec94ec2996e9d
karosi <adkayor21@gmail.com>
2016-11-08 09:21:03 +0100

    Weldone FCC-Timestamp

--- server.js
+++ server.js
@@ -6,8 +6,24 @@ var app = express();
 
 app.set('port',process.env.PORT || 3000)
 app.use(express.static(path.resolve(__dirname,'public')));
-app.get("/", (req,res) => {
-	res.end();
+app.get("/:unixtimestamp", (req,res) => {
+	var time  = moment(req.params.unixtimestamp,"MM DD, YYYY",true)
+
+	if (!time.isValid()) {
+		time = moment.unix(req.params.unixtimestamp);
+	}
+
+	if (!time.isValid()) {
+		res.json({
+			"humanunderstanding":null,
+			"unix":null
+		});
+	}
+	res.json({
+		"humanunderstanding":time.format("MM DD, YYYY"),
+		"unix":time.format("X")
+	});
+	// res.end();
 }).listen(app.get('port'), () => {
 	console.log(`Server running on port ${app.get('port')}`);
 });
